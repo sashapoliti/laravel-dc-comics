e918698c6cec9bf7b034918927b22005a3528f78
sashapoliti <sas.politi@gmail.com>
2024-05-29 12:16:10 +0200

    fix, add request for Store validation

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Comic;
+/* use Illuminate\Support\Facades\Validator; */
+use App\Http\Requests\StoreComicRequest;
 
 class ComicController extends Controller
 {
@@ -34,15 +36,15 @@ class ComicController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreComicRequest $request)
     {
-        $request->validate([
+        /* $request->validate([
             'series' => 'required|max:255|min:3',
             'thumb'=> 'required|max:255|min:3',
             'price'=> 'required|max:20|min:3',
             'type'=> 'required|max:50|min:3',
-        ]);
-        $form_data = $request->all();
+        ]); */
+        $form_data = $request->validated();
         
         /* $new_comic = new Comic(); */
 
@@ -115,4 +117,23 @@ class ComicController extends Controller
         
         return redirect()->route('comics.index');
     }
+
+    /* public function validation($data)
+    {
+        $validator = Validator::make($data, [
+            'series' => 'required|max:255|min:3',
+            'thumb'=> 'required|max:255|min:3',
+            'price'=> 'required|max:20|min:3',
+            'type'=> 'required|max:50|min:3',
+        ],
+        [   
+           'series.required' => 'Hai inserito un campo vuoto!',
+           'thumb.required' => 'Hai inserito un campo vuoto!',
+           'type.required' => 'Hai inserito un campo vuoto!',
+           'price.required' => 'Hai inserito un campo vuoto!',
+           'thumb'=> 'Il campo immagine deve avere un minimo di :min caratteri!',
+        ])->validate();
+
+        return $validator;
+    } */
 }
---
+++ app/Http/Requests/UpdateComicRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateComicRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'series' => 'required|max:255|min:3',
+            'thumb' => 'required|max:255|min:3',
+            'price' => 'required|max:20|min:3',
+            'type' => 'required|max:50|min:3',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'series.required' => 'Il campo :attribute è obbligatorio',
+            'series.min' => 'Il campo :attribute deve avere almeno :min caratteri',
+            'series.max' => 'Il campo :attribute deve avere al massimo :max caratteri',
+            'thumb.required' => 'Il campo :attribute è obbligatorio',
+            'thumb.min' => 'Il campo :attribute deve avere almeno :min caratteri',
+            'thumb.max' => 'Il campo :attribute deve avere al massimo :max caratteri',
+            'price.required' => 'Il campo :attribute è obbligatorio',
+            'price.min' => 'Il campo :attribute deve avere almeno :min caratteri',
+            'price.max' => 'Il campo :attribute deve avere al massimo :max caratteri',
+            'type.required' => 'Il campo :attribute è obbligatorio',
+            'type.min' => 'Il campo :attribute deve avere almeno :min caratteri',
+            'type.max' => 'Il campo :attribute deve avere al massimo :max caratteri',
+        ];
+    }
+}
